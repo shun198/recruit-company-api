875e0f1a76dc90ccee1be92cc90fd822ede2c694
shun198 <shunhiroseluvmri@gmail.com>
2022-11-12 17:12:43 +0900

    xfailとskipのデコレータを追加

--- companies/tests/test_gists.py
+++ companies/tests/test_gists.py
@@ -1,8 +1,31 @@
 from django.test import TestCase
+import pytest
 
 # Create your tests here.
 def test_right_addition() -> None:
     assert 1 == 1
 
+# mark.skipデコレータを入れてテストをスキップ
+@pytest.mark.skip
 def test_wrong_addition() -> None:
+    assert 1 == 2
+
+# 4>1=Trueのためテストをスキップする
+@pytest.mark.skipif(4>1,reason="Skipped because 4>1")
+def test_wrong_addition_if_skipped() -> None:
+    assert 1 == 2
+
+# 0>1=Falseのためテストをスキップしない
+# @pytest.mark.skipif(0>1,reason="Skipped because 4>1")
+# def test_wrong_addition_if_not_skipped() -> None:
+#     assert 1 == 2
+
+# 異常テスト
+# テスト自体は失敗してもいいけどGitHub Actionsを失敗させたくない
+@pytest.mark.xfail
+def test_dont_care_if_fails_right() -> None:
+    assert 1 == 1
+
+@pytest.mark.xfail
+def test_dont_care_if_fails_wrong() -> None:
     assert 1 == 2
